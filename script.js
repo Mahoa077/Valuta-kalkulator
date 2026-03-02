a222c0e5586697017ad431c9ca37f7157feefbc2
Mari <180284303+Mahoa077@users.noreply.github.com>
2026-03-02 08:57:39 +0100

    madag

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 console.log();
 
-const key = '311f91f03c97240a6690c66f';
+const apiKey = '311f91f03c97240a6690c66f';
 
-const convertButton = document.getElementById("convertButton");
+//const convertButton = document.getElementById("convertButton");
 
-convertButton.addEventListener("click", convertCurrency);
+//convertButton.addEventListener("click", convertCurrency);
 
 async function convertCurrency() {
     const amount = document.getElementById("amount").value;
@@ -15,20 +15,29 @@ async function convertCurrency() {
 
 if (amount === "" || amount <= 0){
     result.innerText = "Skriv inn et gyldig beløp";
-    return;
 }
 
-try {
-    const response = await fetch(
-        `https://v6.exchangerate-api.com/v6/${key}/latest/${fromCurrency}`
-    );
+async function convert() {
+    try {
+        const amount = document.getElementById("amount").value;
+        const fromCurrency = document.getElementById("fromCurrency").value;
+        const toCurrency = document.getElementById("toCurrency").value;
+        const result = document.getElementById("result");
 
-    const data = await response.json();
-    const rate = data.conversion_rates[toCurrency];
+        const response = await fetch(
+            `https://v6.exchangerate-api.com/v6/311f91f03c97240a6690c66f/latest/${fromCurrency}`
+        );
     
-    result.innerText = 
-        '${amount} ${fromCurrency} = ${(amount * rate).toFixed(2)} ${toCurrency}';
-
-} catch {
-    result.innerText = "Noe gikk galt. Prøv igjen!";
+        const data = await response.json();
+        const rate = data.conversion_rates[toCurrency];
+        
+        result = amount * rate.toFixed(2)
+            //'${amount} ${fromCurrency} = ${(amount * rate).toFixed(2)} ${toCurrency}';
+            
+        console.log("ferdig ingen error")
+        console.log(result)
+    
+    } catch {
+       console.log("noe gikk feil")
+    }
 }
