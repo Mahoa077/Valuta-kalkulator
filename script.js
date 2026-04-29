ea1250a2e9a24fa09ce80fcc60f486f039ac1422
Mari <github@mari.hoff@outlook.com>
2026-04-29 11:26:04 +0200

    før lunsj onsdag prøvekesamen dag 2

--- script.js
+++ script.js
@@ -2,26 +2,25 @@ console.log("App har startet");
 
 const apiKey = '311f91f03c97240a6690c66f';
 
+//const apiURL = "https://app.exchangerate-api.com/dashboard";
+const CACHE_key = "exchangeRates_";
+const CACHE_tid_key = "exchangeRatesTime_";
+const CACHE_tid = 1000 * 60 * 60; // dette er da altså en time til sammen
+
 //const convertButton = document.getElementById("convertButton");
 
 //convertButton.addEventListener("click", convertCurrency);
 
-async function convertCurrency() {
-    const amount = document.getElementById("amount").value;
-    const fromCurrency = document.getElementById("fromCurrency").value;
-    const toCurrency = document.getElementById("toCurrency").value;
-    const results = document.getElementById("result");
-}
-
-
 async function convert() {
+    const result = document.getElementById("result");
+
     try {
-        const amount = document.getElementById("amount").value;
+        const amount = Number(document.getElementById("amount").value);
         const fromCurrency = document.getElementById("fromCurrency").value;
         const toCurrency = document.getElementById("toCurrency").value;
-        const result = document.getElementById("result");
+        
 
-        if (amount === "" || amount <= 0){
+        if (!amount || amount <= 0){
             result.innerText = "Skriv inn et gyldig beløp";
             return;
         }
@@ -29,53 +28,54 @@ async function convert() {
         const data = await getRates(fromCurrency);
         const rate = data.conversion_rates[toCurrency];
         
-        let output = (amount * rate).toFixed(2);
+        const output = (amount * rate).toFixed(2);
 
         result.innerText = `${amount} ${fromCurrency} = ${output} ${toCurrency}`;
-        const response = await fetch(
-            `https://v6.exchangerate-api.com/v6/311f91f03c97240a6690c66f/latest/${fromCurrency}`
-        );
-    
+        
     } catch (error) {
-       console.log(error)
+       console.log(error);
+       result.innerText = "Noe gikk galt. Prøv igjen senere, unnskyld!";
     }
 
     
 }
 
-const apiURL = "https://app.exchangerate-api.com/dashboard";
-const CACHE_key = "exchangeRates";
-const CACHE_tid_key = "exchangeRatesTime";
-const CACHE_tid = 1000 * 60 * 60; // dette er da altså en time til sammen
 
+// henter valutakurser ( med cache )
 async function getRates(base = "USD") {
-    const cachedData = localStorage.getItem(CACHE_key);
-    const cachedTid = localStorage.getItem(CACHE_tid_key);
+    const cachedData = localStorage.getItem(CACHE_key + base);
+    const cachedTid = localStorage.getItem(CACHE_tid_key + base);
     const now = Date.now();
 
     // dette sjekker om cache eksisterer og om det fortsatt er riktig og funker 
     if(cachedData && cachedTid &&(now - cachedTid < CACHE_tid)) {
-        console.log("Bruker cache");
+        console.log("Bruker cache for", base);
         return JSON.parse(cachedData);
     }
 
     // Ellers så hent ny data
-    console.log("Henter ny data");
+    console.log("Henter ny data for", base);
 
-    const respons = await fetch(
-        `https://v6.exchangerate-api.com/v6/311f91f03c97240a6690c66f/latest/${fromCurrency}`
+    const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${base}`
     );
     
-    const data = await respons.json();
+    const data = await response.json();
+
+    if (data.result === "error"){
+        throw new Error(data["error-type"]);
+    }
+    
 
     // lagrer da til cache 
-    localStorage.setItem(CACHE_key, JSON.stringify(data))
-    localStorage.setItem(CACHE_tid_key, now);
+    localStorage.setItem(CACHE_key + base, JSON.stringify(data));
+    localStorage.setItem(CACHE_tid_key + base, now);
 
     return data;
 }
 
-async function converterCurrency(amount, from, to) {
+/*// konverterer valutaen 
+async function converterCurrency(amount, from, to) {ba
     const data = await getRates();
 
     const rateFRA = data.rates[from];
@@ -86,29 +86,36 @@ async function converterCurrency(amount, from, to) {
     return resultat;
 
     
-}
+} */
 
+// Bilder til valuta kalkulator     
 const selectTo = document.getElementById("toCurrency");
 const bilde = document.getElementById("currencyBilder");
 
-const currencyBilder = {
-    USD: "",
-    EUR: "",
-    NOK: "",
-    AUD: "",
-    CLP: "",
-}
+const bg = document.getElementById("bg");
 
+const currencyBilder = {
+    USD: "bilder/usdbilde.png",
+    EUR: "bilder/eurbilde.png",
+    NOK: "bilder/nokbilde.png",
+    AUD: "bilder/audbilde.png",
+    CLP: "bilder/clpbilde.png",
+}
 
 selectTo.addEventListener("change", () => {
     const valgtCurrency = selectTo.value;
 
     bilde.style.opacity = 0;
 
+    bg.style.opacity = 0;
+
     setTimeout(() => {
+        bg.style.backgroundImage = `url(${currencyBilder[valgtCurrency]})`;
+        bg.style.opacity = 1;
         bilde.src = currencyBilder[valgtCurrency];
         bilde.style.opacity = 1;
     }, 200) ;
 });
 
-
+const convertButton = document.getElementById("convertButton");
+convertButton.addEventListener("click", convert);
