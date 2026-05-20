017c5854a01eb6b43c5b3bac38baaf1156b93c59
Mari <github@mari.hoff@outlook.com>
2026-05-20 10:16:08 +0200

    fiksa ip i javascript

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ async function getRates(base = "USD") {
     console.log("Henter ny data for", base);
 
     const response = await fetch(
-        `http://localhost:3000/api/rates/${base}`
+        `http://192.168.20.77:3000/api/rates/${base}`
     );
     
     const data = await response.json();
