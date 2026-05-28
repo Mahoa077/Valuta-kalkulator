8b732c8b6facd54a6235f8fbe1db796789f7788e
Mari <github@mari.hoff@outlook.com>
2026-05-28 09:26:57 +0200

    legger til cache alder

--- server/server.js
+++ server/server.js
@@ -14,52 +14,112 @@ const CACHE_FILE = path.join(__dirname, "serverCache.json");
 
 let serverCache = {};
 
-// LOADER CACHE
+
+// LAST CACHE FRA FIL
 if (fs.existsSync(CACHE_FILE)) {
+
     const file = fs.readFileSync(CACHE_FILE, "utf-8");
+
     if (file.trim()) {
+
         serverCache = JSON.parse(file);
+
         console.log("Cache lastet fra fil");
     }
 }
 
-// Lagrer CACHE
+
+// LAGRE CACHE
 function saveCache() {
-    fs.writeFileSync(CACHE_FILE, JSON.stringify(serverCache, null, 2));
+
+    fs.writeFileSync(
+        CACHE_FILE,
+        JSON.stringify(serverCache, null, 2)
+    );
+
     console.log("Cache lagret");
 }
 
-// Routing, henter valutakursene, cacher resultatet og deretter sener det tilbake til klienten
+
+// API ROUTE
 app.get("/api/rates/:base", async (req, res) => {
+
     const base = req.params.base;
 
     console.log("Request:", base);
 
+
+    // CACHE HIT
     if (serverCache[base]) {
+
         console.log("CACHE HIT:", base);
-        return res.json(serverCache[base]);
+
+
+        // CACHE ALDER
+        const age = Math.floor(
+
+            (Date.now() -
+            serverCache[base].cachedAt)
+
+            / 1000
+        );
+
+        console.log(
+            "Cache alder:",
+            age,
+            "sekunder"
+        );
+
+
+        return res.json(
+            serverCache[base].data
+        );
     }
 
+
+    // CACHE MISS
     console.log("CACHE MISS:", base);
 
     try {
+
         const response = await fetch(
             `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${base}`
         );
 
         const data = await response.json();
 
-        serverCache[base] = data;
+
+        // LAGRE I CACHE
+        serverCache[base] = {
+
+            data: data,
+
+            cachedAt: Date.now()
+        };
+
+
+        // SKRIV TIL FIL
         saveCache();
 
+
+        // SEND TIL FRONTEND
         return res.json(data);
 
     } catch (err) {
+
         console.log("ERROR:", err);
-        res.status(500).json({ error: "Server error" });
+
+        res.status(500).json({
+            error: "Server error"
+        });
     }
 });
 
+
+// START SERVER
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server kjører på port ${PORT}`);
+
+    console.log(
+        `Server kjører på port ${PORT}`
+    );
 });
